f8b19d6460cc0970837698fb5c7c2ba33dca5971
rajotiyapawan <rajotiyapawan@gmail.com>
2025-08-27 06:48:14 +0530

    Clean Architecture

--- pokedex/src/main/java/com/rajotiyapawan/pokedex/domain/model/RequestModels.kt
+++ pokedex/src/main/java/com/rajotiyapawan/pokedex/domain/model/RequestModels.kt
@@ -3,6 +3,6 @@ package com.rajotiyapawan.pokedex.domain.model
 data class RequestModel(
     val name: String? = null,
     val url: String? = null,
-    val offset: Int? = null,
-    val limit: Int? = null
+    val offset: Int? = 0,
+    val limit: Int? = 2000
 )
